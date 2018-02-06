fb7a30fc41937cdacbdccc553cc28e8c78660ef7
fedeaz <fedeaz17@hotmail.com>
2018-02-06 10:08:18 -0300

    completados ejercicios 5 6 7 y 8

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -3,22 +3,58 @@ transformarlos a enteros (parseInt),realizar la operación correcta y
 mostar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
-{	
+{	var numeroUno;
+    var numeroDos;
+    var suma;
+    numeroUno = document.getElementById("numeroUno").value;
+    numeroDos = document.getElementById("numeroDos").value;
+    numeroUno = parseInt(numeroUno);
+    numeroDos = parseInt(numeroDos);
+    suma = numeroUno + numeroDos;
+    alert("la suma es " + suma);
+
 	
 }
 
 function restar()
 {
+    var numeroUno;
+    var numeroDos;
+    var resta;
+    numeroUno = document.getElementById("numeroUno").value;
+    numeroDos = document.getElementById("numeroDos").value;
+    numeroUno = parseInt(numeroUno);
+    numeroDos = parseInt(numeroDos);
+    resta = numeroUno - numeroDos;
+    alert("la resta es " + resta);
 	
 }
 
 function multiplicar()
 { 
+    var numeroUno;
+    var numeroDos;
+    var multi;
+    numeroUno = document.getElementById("numeroUno").value;
+    numeroDos = document.getElementById("numeroDos").value;
+    numeroUno = parseInt(numeroUno);
+    numeroDos = parseInt(numeroDos);
+    multi = numeroUno * numeroDos;
+    alert("la multiplicaciòn es " + multi);
 	
 }
 
 function dividir()
 {
+    var numeroUno;
+    var numeroDos;
+    var divi;
+    numeroUno = document.getElementById("numeroUno").value;
+    numeroDos = document.getElementById("numeroDos").value;
+    numeroUno = parseInt(numeroUno);
+    numeroDos = parseInt(numeroDos);
+    divi = numeroUno / numeroDos;
+    alert("la diviciòn es " + divi);
 	
 }
 
