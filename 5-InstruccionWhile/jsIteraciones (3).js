fd04f0732b6f430c4c373ed56c21d1d6b110705c
fedeaz <fedeazlol@hotmail.com>
2018-02-19 12:21:38 -0300

    ejercicios while hasta el 5

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -1,7 +1,16 @@
 function Mostrar()
 {
 
-var clave = prompt("ingrese el número clave.");
+var clave = prompt("ingrese el número clave.")
+while (clave != "utn750" && clave != null)
+{
+clave = prompt("Error igrese su calave")
+if (clave == null)
+{
+    break;  
+}
+
+}
 
 
 }//FIN DE LA FUNCIÓN
