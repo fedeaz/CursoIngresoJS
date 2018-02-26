8326cb56d2c5223113936e62f03d5a574182b7e9
fedeaz <fedeazlol@hotmail.com>
2018-02-26 10:37:53 -0300

    TP 12

--- 8-TPs/jsRisingBTL.js
+++ 8-TPs/jsRisingBTL.js
@@ -9,5 +9,115 @@ F.	Nacionalidad, “A” para argentinos, “E” para extranjeros, “N” para
  */
 function ComenzarIngreso () 
 {
- 
+
+    var edad = "si";
+    var sexo = prompt("ingrese su sexo con m o f")
+    var estadoc = "si"
+    var sueldo = "si"
+    var legajo
+    var nacion
+
+    
+      while(edad == "si")
+    {
+        edad = prompt("ingrese una edad");
+        edad = parseInt(edad);
+        while (isNaN(edad))
+		{
+            alert("ingrese una edad valida");
+            edad = prompt("ingrese una edad");
+            edad = parseInt(edad);
+            continue
+		} 
+
+        if(edad >= 18 || edad < 90)
+    {
+        document.getElementById("Edad").value = edad;
+    }
+    else
+     {  
+        alert("ingrese una edad valida");
+        edad = prompt("");
+        continue
+        
+     }
+   }
+   while(sexo != "f" && sexo != "m")
+   {
+       alert("Ingrese un sexo valido");
+       sexo = prompt("ingrese su sexo con m o f");
+       continue 
+   }
+   document.getElementById("Sexo").value = sexo;
+
+   while(estadoc == "si")
+   {
+       estadoc = prompt("ingresar estado civil: 1-para soltero, 2-para casados, 3-para divorciados y 4-para viudos ");
+       estadoc = parseInt(estadoc);
+       while(isNaN(estadoc))
+       {
+           alert("ingrese un Estado civil correcto");
+           estadoc = prompt("ingresar estado civil: 1-para soltero, 2-para casados, 3-para divorciados y 4-para viudos ");
+           estadoc = parseInt(estadoc);
+       }
+       if(estadoc === 1)
+       {
+           estadoc = "soltero";
+           //document.getElementById("EstadoCivil").value = estadoc;
+       }
+       else if(estadoc === 2)
+       {
+           estadoc = "casados";
+           //document.getElementById("EstadoCivil"),value = estadoc;
+       }
+       else if(estadoc === 3)
+       {
+           estadoc = "divorciados";
+          // document.getElementById("EstadoCivil"),value = estadoc;
+       }
+       else if(estadoc === 4)
+       {
+           estadoc = "viudos";
+           //document.getElementById("EstadoCivil"),value = estadoc;
+       }
+       document.getElementById("EstadoCivil").value = estadoc;
+    
+       }
+       while (estadoc >4)
+       {
+           alert("ingrese un Estado Civil correcto");
+            estadoc = prompt("ingresar estado civil: 1-para soltero, 2-para casados, 3-para divorciados y 4-para viudos ");
+            estadoc = parseInt(estadoc);
+           continue
+       }
+   
+
+   while(sueldo == "si")
+   {
+       sueldo = prompt("ingrese su sueldo bruto");
+       sueldo = parseInt(sueldo);
+       while (isNaN(sueldo))
+       {
+           alert("ingrese un numero valido");
+           sueldo = prompt("ingrese su sueldo bruto");
+           sueldo = parseInt(sueldo);
+           continue
+       }
+       while (sueldo <8000)
+       {
+           alert("el sueldo bruto no puede se menor a $8000");
+           sueldo = prompt("ingrese su sueldo bruto");
+           sueldo = parseInt(sueldo);
+           continue
+       }
+       while(sueldo >= 8000)
+       {
+           document.getElementById("Sueldo").value = sueldo;
+       }
+
+   }
+
+   while ()
+
+
 }
